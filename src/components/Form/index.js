4b121ae1664649eeb452a22b0b083b07911949bc
Brian Lucca <brianlucca2002@gmail.com>
2023-12-19 00:26:10 -0300

    Primeiro Commit

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,23 +1,44 @@
-import React from "react"
-import { View, Text, TextInput, Button } from "react-native"
-import ResultImc from "./resultImc";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import ImcResult from "./resultImc";
 
 export default function Form() {
-    return(
-        <View>
-            <View>
-                <Text>Altura</Text>
-                <TextInput  
-                placeholder="EX: 1.75" 
-                keyboardType="numeric"/>
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [messageImc, setMessageImc] = useState("Insira o Peso e a Altura");
+  const [imc, setImc] = useState(null);
+  const [textButton, setTextButton] = useState("Calcular");
+  
+  const imcCalculator = () => {
+    return setImc((weight / (height * height)).toFixed(2));
+  };
 
-                <Text>Peso</Text>
-                <TextInput
-                placeholder="EX: 80.00" 
-                keyboardType="numeric"/>
-                <Button title="Calcular"/>
-            </View>
-            {/* <ResultImc  messageResultImc={messageImc} ResultImc={imc}/> */}
-        </View>
-    );
+  const validationImc = () => {
+    if(weight != null && height != null) {
+        imcCalculator()
+        setWeight(null)
+        setHeight(null)
+        setMessageImc('Seu Imc é igual:')
+        setTextButton('Calcular Novamente')
+        return
+    }
+    setImc(null)
+    setTextButton('Calcular')
+    setMessageImc('Insira o peso e altura')
+  }
+
+  return (
+    <View>
+      <View>
+        <Text>Altura</Text>
+        <TextInput onChangeText={setHeight} value={height} placeholder="EX: 1.75" keyboardType="numeric" />
+
+        <Text>Peso</Text>
+        <TextInput onChangeText={setWeight} value={weight} placeholder="EX: 80.00" keyboardType="numeric" />
+        <Button onPress={() => validationImc()}
+        title="Calcular Imc" />
+      </View>
+      <ImcResult messageResultImc={messageImc} ResultImc={imc} />
+    </View>
+  );
 }
