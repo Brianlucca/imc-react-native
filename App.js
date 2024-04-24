3caffbf47aeee48074356c6726791c97d5683ba8
Brianlucca <brianlucca2002@gmail.com>
2024-04-24 13:43:20 -0300

    fix: header component comes after the router

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { NativeBaseProvider } from 'native-base'
 import React from 'react'
 import Menu from './src/components/menu'
 import Routes from './src/routes'
+import Header from './src/components/header'
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
         <Routes />
+        <Header />
         <Menu />
       </NavigationContainer>
     </NativeBaseProvider>
