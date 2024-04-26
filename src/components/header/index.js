a40e897ba74058d51e3999a76af581bd5b02cf3a
Brianlucca <brianlucca2002@gmail.com>
2024-04-26 00:50:37 -0300

    feat: added link component for social networks.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,27 @@
 import { FontAwesome5 } from '@expo/vector-icons'
-import { Box } from 'native-base'
+import { Box, Link } from 'native-base'
 import styleHeader from './style'
 
 function Header() {
   return (
     <Box style={styleHeader.container}>
       <Box style={styleHeader.content}>
-        <FontAwesome5
-          name="linkedin-in"
-          size={24}
-          color="white"
-          style={styleHeader.icon}
-        />
-        <FontAwesome5
-          name="github"
-          size={24}
-          color="white"
-          style={styleHeader.icon}
-        />
+        <Link href="https://www.linkedin.com/in/brian-lucca-cardozo-a2a134297/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+          <FontAwesome5
+            name="linkedin-in"
+            size={24}
+            color="white"
+            style={styleHeader.icon}
+          />
+        </Link>
+        <Link href="https://github.com/Brianlucca">
+          <FontAwesome5
+            name="github"
+            size={24}
+            color="white"
+            style={styleHeader.icon}
+          />
+        </Link>
       </Box>
     </Box>
   )
