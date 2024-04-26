c9ec7cfeef51faea793c3066bf5db7577726f3a9
Brianlucca <brianlucca2002@gmail.com>
2024-04-26 00:51:32 -0300

    feat: adding the navigate link and import component to navigate between screens.

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,14 +1,34 @@
 import { FontAwesome5 } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 import { Box } from 'native-base'
 import styleMenu from './style'
 
 function Menu() {
+  const navigation = useNavigation()
+
+  const navigateAbout = () => {
+    navigation.navigate('About')
+  }
+  const navigateForm = () => {
+    navigation.navigate('Form')
+  }
+
   return (
     <Box style={styleMenu.container}>
       <Box style={styleMenu.content}>
-        <FontAwesome5 name="home" size={24} color="white" />
+        <FontAwesome5
+          name="home"
+          size={24}
+          color="white"
+          onPress={navigateForm}
+        />
         <FontAwesome5 name="address-book" size={24} color="white" />
-        <FontAwesome5 name="question-circle" size={24} color="white" />
+        <FontAwesome5
+          name="question-circle"
+          size={24}
+          color="white"
+          onPress={navigateAbout}
+        />
       </Box>
     </Box>
   )
